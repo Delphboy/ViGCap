c798b9d222da7f08d24e0d1f6431e2684aeb3fd8
Henry <hps1945@live.co.uk>
2023-10-31 23:07:59 +0000

    for ITS

--- hpc/submitter_andrena.sh
+++ hpc/submitter_andrena.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-declare learning_rates=(0.0001 0.001 0.01 0.1 1)
+declare learning_rates=(0.0001 0.002)
 declare annealing_factors=(0.1 0.5 0.9)
 
 for lr in "${learning_rates[@]}"
 do
     for af in "${annealing_factors[@]}"
     do
-        qsub -v lr=$lr,af=$af -N "flickr8k_lr${lr}_af${af}" train.qsub 
+        qsub -v lr=$lr,af=$af -N "coco_lr${lr}_af${af}" train.qsub 
     done
 done
 
